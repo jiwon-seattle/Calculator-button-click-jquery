08efea4348df051af88ed6870a02875f96990c2a
Jiwon Han <jiwon1.han@gmail.com>
2020-01-16 11:04:45 -0800

    Add other functions such as subtract, multifly and divde including add

--- js/scripts.js
+++ js/scripts.js
@@ -15,12 +15,35 @@ var divide = function(number1, number2) {
 };
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("button#add").click(function(event) {
 
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
+    var number1 = parseInt($("#num1").val());
+    var number2 = parseInt($("#num2").val());
     var result = add(number1, number2);
     $("#output").text(result);
     event.preventDefault();
   });
+   $("button#subtract").click(function(event) {
+
+     var number1 = parseInt($("#num1").val());
+     var number2 = parseInt($("#num2").val());
+     var result = subtract(number1, number2);
+     $("#output").text(result);
+     event.preventDefault();
+   });
+   $("button#multiply").click(function(event) {
+
+     var number1 = parseInt($("#num1").val());
+     var number2 = parseInt($("#num2").val());
+     var result = multiply(number1, number2);
+     $("#output").text(result);
+     event.preventDefault();
+   });
+   $("button#divide").click(function(event) {
+     var number1 = parseInt($("#num1").val());
+     var number2 = parseInt($("#num2").val());
+     var result = divide(number1, number2);
+     $("#output").text(result);
+     event.preventDefault();
+   });
 });
